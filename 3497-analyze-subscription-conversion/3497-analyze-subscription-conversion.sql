d541f9d78605c19735a085ed619d1d533585c5f3
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2025-10-15 18:23:54 +0530

    Time: 274 ms (90.96%), Space: 0B (100%) - LeetHub

--- 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
+++ 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
@@ -5,6 +5,5 @@ ROUND(SUM(CASE WHEN activity_type = "paid" THEN activity_duration ELSE 0 END) /
 SUM(CASE WHEN activity_type  = "paid" THEN 1 ELSE 0 END), 2) as paid_avg_duration
 FROM UserActivity
 GROUP BY user_id
-HAVING SUM(CASE WHEN activity_type  = "free_trial" THEN 1 ELSE 0 END) > 0 AND 
-SUM(CASE WHEN activity_type  = "paid" THEN 1 ELSE 0 END) > 0
+HAVING SUM(activity_type = 'paid') > 0
 ORDER BY user_id;
