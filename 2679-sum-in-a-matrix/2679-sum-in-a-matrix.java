e1a683c57ed53701b2181f44b2854bc427da2728
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2025-10-27 18:03:17 +0530

    Time: 17 ms (20%), Space: 58.3 MB (97.96%) - LeetHub

--- 2679-sum-in-a-matrix/2679-sum-in-a-matrix.java
+++ 2679-sum-in-a-matrix/2679-sum-in-a-matrix.java
@@ -1,14 +1,17 @@
 class Solution {
     public int matrixSum(int[][] nums) {
         int sum = 0;
-        int i = 1;
-        for(i = 1; i <= nums[0].length;i++){
-            int max = 0;
-            for(int[] ar : nums){
+        ArrayList<int[]> al = new ArrayList();
+        for(int ar[]:nums){
             Arrays.sort(ar);
-            max = Math.max(max, ar[ar.length - i]);
+            al.add(ar);
+        }
+        for(int i = 1; i <= nums[0].length; i++){
+            int max = 0;
+            for(int[] ar:al){
+                max = (max < ar[ar.length - i]) ? ar[ar.length - i] : max;
             }
-            sum += max;
+            sum = sum + max;
         }
         return sum;
     }
