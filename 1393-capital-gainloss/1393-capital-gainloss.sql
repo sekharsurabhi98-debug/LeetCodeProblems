6d53d8c186b50bf15a43996d4f639afb62d24f09
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2025-10-28 21:12:02 +0530

    Time: 476 ms (66.83%), Space: 0B (100%) - LeetHub

---
+++ 1393-capital-gainloss/1393-capital-gainloss.sql
@@ -0,0 +1,4 @@
+SELECT stock_name, SUM(CASE WHEN operation = 'Sell' THEN price ELSE 0 END) - 
+SUM(CASE WHEN operation = 'Buy' THEN price ELSE 0 END) as capital_gain_loss
+FROM Stocks
+GROUP BY stock_name;
