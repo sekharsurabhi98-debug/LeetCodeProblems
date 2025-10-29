75e89783b71793bf0118476dc9e668277ff796b1
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2025-10-29 19:12:17 +0530

    Time: 1 ms (33.25%), Space: 41.2 MB (16.75%) - LeetHub

--- 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
@@ -1,7 +1,7 @@
 class Solution {
     public int smallestNumber(int n) {
-        if((n & (n+1)) == 0) return n;
-        StringBuilder sb = new StringBuilder();
+        /*if((n & (n+1)) == 0) return n;
+        //StringBuilder sb = new StringBuilder();
         int c = 0;
         while(n > 0){
             int r = n % 2;
@@ -9,14 +9,18 @@ class Solution {
             n = n / 2;
             c++;
         }
-        for(int i = 0; i < sb.length(); i++){
-            if(sb.charAt(i) == '0') sb.setCharAt(i,'1');
-        }
-        int res = 0;
-        for(int i = sb.length() - 1; i >= 0; i--){
-            res += Math.pow(2,i) * (sb.charAt(i) - 48);
-        }
-        return res;
-        //return (int)Math.pow(2,Integer.toBinaryString(n).length())-1;
+        // for(int i = 0; i < sb.length(); i++){
+        //     if(sb.charAt(i) == '0') sb.setCharAt(i,'1');
+        // }
+        // int res = 0;
+        // for(int i = sb.length() - 1; i >= 0; i--){
+        //     res += Math.pow(2,i) * (sb.charAt(i) - 48);
+        // }
+        //return res;
+        return (int)Math.pow(2,Integer.toBinaryString(n).length())-1;*/
+        int x = n;
+        while((x & (x + 1)) != 0)
+           x++;
+        return x;
     }
 }
