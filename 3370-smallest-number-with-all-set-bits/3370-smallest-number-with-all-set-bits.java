eafa55eb6860374e4cd0757c10a31c0404f787e2
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2025-10-29 19:08:08 +0530

    Time: 1 ms (33.25%), Space: 40.8 MB (46.73%) - LeetHub

--- 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
@@ -1,20 +1,20 @@
 class Solution {
     public int smallestNumber(int n) {
         if((n & (n+1)) == 0) return n;
-        StringBuilder sb = new StringBuilder();
-        int c = 0;
-        while(n > 0){
-            int r = n % 2;
-            sb.append(r);
-            n = n / 2;
-            c++;
-        }
+        // StringBuilder sb = new StringBuilder();
+        // int c = 0;
+        // while(n > 0){
+        //     int r = n % 2;
+        //     sb.append(r);
+        //     n = n / 2;
+        //     c++;
+        // }
         // for(int i = 0; i < sb.length(); i++){
         //     if(sb.charAt(i) == '0') sb.setCharAt(i,'1');
         // }
         // for(int i = sb.length() - 1; i >= 0; i--){
         //     res += 2 ** 
         // }
-        return (int)Math.pow(2,c) - 1;
+        return (int)Math.pow(2,Integer.toBinaryString(n).length())-1;
     }
 }
