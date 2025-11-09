474b4fbe20d3dc0ebf45c2e067e1729d032ae765
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2025-11-09 09:59:12 +0530

    Time: 2760 ms (7.94%), Space: 46.7 MB (5.68%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int operations = 0;
+        while(num1 != 0 && num2 != 0){
+            if(num1 >= num2)
+                num1 = num1 - num2;
+            else
+                num2 = num2 - num1;
+            System.out.println(num1);
+            System.out.println(num2);
+            operations++;
+        }
+        return operations;
+    }
+}
