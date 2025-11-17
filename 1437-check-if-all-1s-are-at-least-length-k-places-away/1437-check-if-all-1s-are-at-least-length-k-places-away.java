c249b0e3405a53f005bb86879715be64373aecc4
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2025-11-17 07:45:45 +0530

    Time: 2 ms (26%), Space: 65.4 MB (15.14%) - LeetHub

--- 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,18 +1,13 @@
 class Solution {
     public boolean kLengthApart(int[] nums, int k) {
         int zeroCount = 0;
-        boolean first = false;
+        int lastOneIdx = -1;
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 1 && i > 0 && first){
-                if(zeroCount < k)
-                   return false;
-                zeroCount = 0;
-            }else if(nums[i] == 0){
-                zeroCount++;
-            }else if(nums[i] == 1 && !first){
-                first = true;
-                zeroCount = 0;
+            if(nums[i] == 1 && lastOneIdx != -1){
+                if(i - lastOneIdx - 1 < k) return false;
+                
             }
+            if(nums[i] == 1) lastOneIdx = i;
         }
         return true;
         
