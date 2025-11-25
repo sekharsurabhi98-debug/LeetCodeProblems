60263e4f7f703a1fc3f8a7d6e50d54edf286d5c1
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2025-11-25 18:39:56 +0530

    Time: 315 ms (11.82%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,2 +1,7 @@
-SELECT MAX(SALARY) as SecondHighestSalary FROM employee
-WHERE salary < (select MAX(salary) FROM employee);
+-- SELECT salary , dense_rank() over(ORDER BY salary desc) as  SecondHighestSalary FROM employee
+-- limit 1 offset 1;
+
+SELECT max(salary) as SecondHighestSalary FROM Employee
+WHERE salary < (
+    SELECT max(salary) FROM Employee
+)
