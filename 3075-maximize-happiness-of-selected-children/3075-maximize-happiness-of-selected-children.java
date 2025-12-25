5e17f5b1c298586cfe063648fd0963750dd5eb98
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2025-12-25 22:14:41 +0530

    Time: 43 ms (43.55%), Space: 109.1 MB (51.08%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -6,9 +6,6 @@ class Solution {
         int idx = happiness.length - 1;
         int decrement = 1;
         while(k > 0){
-           
-
-           System.out.println(happiness[idx]+":"+idx);
            max += happiness[idx];
            idx--;
            if(idx >= 0 && happiness[idx] - decrement >= 0)
