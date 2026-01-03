433a6987db33ede9ea61f35bc24e182d45ca5c46
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2026-01-03 08:59:02 +0530

    Time: 5 ms (12.31%), Space: 49.7 MB (9.28%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,36 +1,33 @@
 class Solution {
 
-    void dfs(int[][] graph, boolean[] visited, int v){
-
-        Stack<Integer> stack = new Stack();
-        stack.push(v);
-
-        while(!stack.isEmpty()){
-
-            int u = stack.pop();
-    
-                visited[u] = true;
-                for(int i = 0; i < graph.length; i++){
-                    if(graph[u][i] == 1 && !visited[i]){
-                        stack.push(i);
-                    }
-            }
-        }
-    }
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         boolean[] visited = new boolean[n];
         int count = 0;
-
-        for(int v = 0; v < n; v++){
-
-            if(!visited[v]){
-
-                dfs(isConnected, visited, v);
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
                 count++;
-
+                bfs(isConnected,i,visited);
             }
         }
         return count;
+    }
+
+    private void bfs(int[][] grid, int v, boolean[] visited){
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(v);
+
+        while(!queue.isEmpty()){
+
+            int u = queue.poll();
+            visited[u] = true;
+            for(int i = 0; i < grid.length; i++){
+                if(grid[u][i] == 1 && !visited[i]){
+                    queue.offer(i);
+                }
+            }
+        }
     }
 }
