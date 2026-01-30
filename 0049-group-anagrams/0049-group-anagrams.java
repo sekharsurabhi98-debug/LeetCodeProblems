74402f0d0890f0bff8dae329c9c6c1b37a69e9d7
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2026-01-30 08:40:00 +0530

    Time: 7 ms (76.24%), Space: 49.8 MB (46.74%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,14 +1,14 @@
 class Solution {
-    public static boolean isAnagram(String s1, String s2){
-        char ar1[] = s1.toCharArray();
-        char ar2[] = s2.toCharArray();
-        Arrays.sort(ar1);
-        Arrays.sort(ar2);
-        s1 = new String(ar1);
-        s2 = new String(ar2);
-        return (s1.equals(s2));
+    // public static boolean isAnagram(String s1, String s2){
+    //     char ar1[] = s1.toCharArray();
+    //     char ar2[] = s2.toCharArray();
+    //     Arrays.sort(ar1);
+    //     Arrays.sort(ar2);
+    //     s1 = new String(ar1);
+    //     s2 = new String(ar2);
+    //     return (s1.equals(s2));
         
-    }
+    // }
     public List<List<String>> groupAnagrams(String[] strs) {
         
         /*List<List<String>> result = new ArrayList<>();
@@ -32,17 +32,74 @@ class Solution {
         }
         return result;*/
 
-        HashMap<String, List<String>> map = new HashMap<String,List<String>>();
-        for(String s : strs){
-            char[] ar = s.toCharArray();
+        // HashMap<String, List<String>> map = new HashMap<String,List<String>>();
+        // for(String s : strs){
+        //     char[] ar = s.toCharArray();
+        //     Arrays.sort(ar);
+        //     String s1 = String.valueOf(ar);
+        //     if(!map.containsKey(s1))
+        //          map.put(s1, new ArrayList<String>());
+        //     map.get(s1).add(s);
+        // }
+        // return new ArrayList<>(map.values());
+
+    //     List<List<String>> result = new ArrayList<>();
+    //     for(int i = 0; i < strs.length; i++){
+    
+    //         if(strs[i].equals("taken")) continue;
+    //         List<String> list = new ArrayList<>();
+    //         int j = i+1;
+    //         list.add(strs[i]);
+    //         while(j < strs.length){
+    //             if(isValid(strs[i], strs[j])){
+    //                list.add(strs[j]);
+    //                strs[j] = "taken";
+    //             }
+    //             j++;
+    //         }
+
+    //          result.add(list);
+    //     }
+
+    //    return result;
+
+        Map<String, List<String>> map = new HashMap<>();
+        for(int i = 0; i < strs.length; i++){
+            
+            char[] ar = strs[i].toCharArray();
             Arrays.sort(ar);
-            String s1 = String.valueOf(ar);
-            if(!map.containsKey(s1))
-                 map.put(s1, new ArrayList<String>());
-            map.get(s1).add(s);
+
+            String s = new String(ar);
+
+            if(map.containsKey(s)){
+                map.get(s).add(strs[i]);
+            }else{
+                map.put(s, new ArrayList<>());
+                map.get(s).add(strs[i]);
+            }
         }
-        System.out.println(map);
         return new ArrayList<>(map.values());
-        
     }
+    
+    public int[] calcFreq(String s){
+
+        int[] freq = new int[26];
+        for(char c : s.toCharArray()){
+            int idx = c - 97;
+            freq[idx]++;
+        }
+        return freq;
+    }
+
+    public boolean isValid(String s1, String s2){
+
+        int[] freq1 = calcFreq(s1);
+        int[] freq2 = calcFreq(s2);
+        for(int i = 0; i < 26; i++){
+            if(freq1[i] != freq2[i])
+            return false;
+        }
+        return true;
+    }
+    
 }
