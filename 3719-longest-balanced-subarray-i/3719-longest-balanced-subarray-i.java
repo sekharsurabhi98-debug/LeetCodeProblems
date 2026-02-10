8f4ee15d62bd01fd3be0c45e5b840e206bd191ad
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2026-02-10 10:33:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
@@ -1,22 +1,21 @@
 class Solution {
     public int longestBalanced(int[] nums) {
-        int n = nums.length;
-        int maxLen = 0;
-        
-        for (int i = 0; i < n; i++) {
-            HashSet<Integer> evens = new HashSet<>();
-            HashSet<Integer> odds = new HashSet<>();
-            
-            for (int j = i; j < n; j++) {
-                if (nums[j] % 2 == 0)
-                    evens.add(nums[j]);
-                else
-                    odds.add(nums[j]);
-                
-                if (evens.size() == odds.size())
-                    maxLen = Math.max(maxLen, j - i + 1);
-            }
-        }
-        return maxLen;
+       int maxLength = 0;
+
+       for(int i = 0; i < nums.length; i++){
+       Set<Integer> even = new HashSet<>();
+       Set<Integer> odd = new HashSet<>();
+          for(int j = i; j < nums.length; j++){
+               
+               if(!(even.contains(nums[j])) && nums[j] % 2 == 0)
+                   even.add(nums[j]);
+                else if(!(odd.contains(nums[j])) && nums[j] % 2 == 1)
+                   odd.add(nums[j]);
+                if(even.size() == odd.size())
+                    maxLength = Math.max(maxLength, j - i + 1);
+                System.out.println(even+" : "+odd);
+          }
+       }
+       return maxLength;
     }
 }
