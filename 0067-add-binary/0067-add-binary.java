0c9584d431a0892a487135af6ab6e51bd4f2a3ab
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2026-02-15 11:33:08 +0530

    Time: 2 ms (49.49%), Space: 43.8 MB (25.29%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,94 +1,122 @@
 class Solution {
     public String addBinary(String a, String b) {
 
-        String s = "";
-        char c = '0';
-        int l = a.length() - 1;
-        int r = b.length() - 1;
-        int count = 1;
-        while(l >= 0 || r >= 0 || c == '1'){
+        // String s = "";
+        // char c = '0';
+        // int l = a.length() - 1;
+        // int r = b.length() - 1;
+        // int count = 1;
+        // while(l >= 0 || r >= 0 || c == '1'){
           
       
+        //BRUTE FORCE ALL CONSIDERING ALL POSSIBLE SCENARIOS
+        //     if(l >= 0 && r >= 0 && a.charAt(l) == '1' && b.charAt(r) == '1' && c == '1'){
+        //         s = '1' + s;
+        //         c = '1';
+        //     }
+        //     else if(l >= 0 && r >= 0 && a.charAt(l) == '1' && b.charAt(r) == '0' && c == '1'){
+        //         s = '0' + s;
+        //         c = '1';
+        //     }
+        //     else if(l >= 0 && r >= 0 && a.charAt(l) == '1' && b.charAt(r) == '1' && c == '0'){
+        //         s = '0' + s;
+        //         c = '1';
+        //     }
+        //     else if(l >= 0 && r >= 0 && a.charAt(l) == '0' && b.charAt(r) == '1' && c == '0'){
+        //         s = '1' + s;
+        //         c = '0';
+        //     }
+        //     else if(l >= 0 && r >= 0 && a.charAt(l) == '1' && b.charAt(r) == '0' && c == '0'){
+        //         s = '1' + s;
+        //         c = '0';
+        //     }
+        //      else if(l >= 0 && r >= 0 && a.charAt(l) == '0' && b.charAt(r) == '0' && c == '0'){
+        //         s = '0' + s;
+        //         c = '0';
+        //     }
+        //     else if(l >= 0 && r >= 0 && a.charAt(l) == '0' && b.charAt(r) == '0' && c == '1'){
+        //         s = '1' + s;
+        //         c = '0';
+        //     }
+
+        //     else if(l >= 0 && r >= 0 && a.charAt(l) == '0' && b.charAt(r) == '1' && c == '1'){
+        //         s = '0' + s;
+        //         c = '1';
+        //     }
+
+        //     else if(l < 0 && r >= 0 && b.charAt(r) == '0' && c == '1'){
+        //         s = '1' + s;
+        //         c = '0';
+        //     }
+
+        //     else if(l < 0 && r >= 0 && b.charAt(r) == '0' && c == '0'){
+        //         s = '0' + s;
+        //         c = '0';
+        //     }
+
+        //      else if(l < 0 && r >= 0 && b.charAt(r) == '1' && c == '1'){
+        //         s = '0' + s;
+        //         c = '1';
+        //     }
+
+        //      else if(l < 0 && r >= 0 && b.charAt(r) == '1' && c == '0'){
+        //         s = '1' + s;
+        //         c = '0';
+        //     }
         
-            if(l >= 0 && r >= 0 && a.charAt(l) == '1' && b.charAt(r) == '1' && c == '1'){
-                s = '1' + s;
-                c = '1';
-            }
-            else if(l >= 0 && r >= 0 && a.charAt(l) == '1' && b.charAt(r) == '0' && c == '1'){
-                s = '0' + s;
-                c = '1';
-            }
-            else if(l >= 0 && r >= 0 && a.charAt(l) == '1' && b.charAt(r) == '1' && c == '0'){
-                s = '0' + s;
-                c = '1';
-            }
-            else if(l >= 0 && r >= 0 && a.charAt(l) == '0' && b.charAt(r) == '1' && c == '0'){
-                s = '1' + s;
-                c = '0';
-            }
-            else if(l >= 0 && r >= 0 && a.charAt(l) == '1' && b.charAt(r) == '0' && c == '0'){
-                s = '1' + s;
-                c = '0';
-            }
-             else if(l >= 0 && r >= 0 && a.charAt(l) == '0' && b.charAt(r) == '0' && c == '0'){
-                s = '0' + s;
-                c = '0';
-            }
-            else if(l >= 0 && r >= 0 && a.charAt(l) == '0' && b.charAt(r) == '0' && c == '1'){
-                s = '1' + s;
-                c = '0';
+        //     else if(l >= 0 && r < 0 && a.charAt(l) == '1' && c == '0'){
+        //         s = '1' + s;
+        //         c = '0';
+        //     }
+        //     else if(l >= 0 && r < 0 && a.charAt(l) == '1' && c == '1'){
+        //         s = '0' + s;
+        //         c = '1';
+        //     }
+        //     else if(l >= 0 && r < 0 && a.charAt(l) == '0' && c == '0'){
+        //         s = '0' + s;
+        //         c = '0';
+        //     }
+        //     else if(l >= 0 && r < 0 && a.charAt(l) == '0' && c == '1'){
+        //         s = '1' + s;
+        //         c = '0';
+        //     }
+        //     else if(l < 0 && r < 0 && c == '1'){
+        //         s = '1' + s;
+        //         c = '0';
+        //     }
+        //     System.out.println(count++);
+
+        //     r--;
+        //     l--;
+        // }
+        // return s;
+
+
+        int l = a.length() - 1;
+        int r = b.length() - 1;
+        int sum = 0;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while(l >= 0 || r >= 0 || carry == 1){
+            
+            sum += carry;
+            if(l >= 0){
+
+                sum += a.charAt(l) - '0';
+                l--;
             }
 
-            else if(l >= 0 && r >= 0 && a.charAt(l) == '0' && b.charAt(r) == '1' && c == '1'){
-                s = '0' + s;
-                c = '1';
+            if(r >= 0){
+
+                sum += b.charAt(r) - '0';
+                r--;
             }
 
-            else if(l < 0 && r >= 0 && b.charAt(r) == '0' && c == '1'){
-                s = '1' + s;
-                c = '0';
-            }
-
-            else if(l < 0 && r >= 0 && b.charAt(r) == '0' && c == '0'){
-                s = '0' + s;
-                c = '0';
-            }
-
-             else if(l < 0 && r >= 0 && b.charAt(r) == '1' && c == '1'){
-                s = '0' + s;
-                c = '1';
-            }
-
-             else if(l < 0 && r >= 0 && b.charAt(r) == '1' && c == '0'){
-                s = '1' + s;
-                c = '0';
-            }
-        
-            else if(l >= 0 && r < 0 && a.charAt(l) == '1' && c == '0'){
-                s = '1' + s;
-                c = '0';
-            }
-            else if(l >= 0 && r < 0 && a.charAt(l) == '1' && c == '1'){
-                s = '0' + s;
-                c = '1';
-            }
-            else if(l >= 0 && r < 0 && a.charAt(l) == '0' && c == '0'){
-                s = '0' + s;
-                c = '0';
-            }
-            else if(l >= 0 && r < 0 && a.charAt(l) == '0' && c == '1'){
-                s = '1' + s;
-                c = '0';
-            }
-            else if(l < 0 && r < 0 && c == '1'){
-                s = '1' + s;
-                c = '0';
-            }
-            System.out.println(count++);
-
-            r--;
-            l--;
+            sb.append(sum % 2);
+            carry = sum / 2;
+            sum = 0;
         }
-        return s;
+        return sb.reverse().toString();
     }
 } 
