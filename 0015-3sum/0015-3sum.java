b673f72a6960d8efd9106db096c20a03da5b4066
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2026-02-16 21:43:32 +0530

    Time: 40 ms (22.72%), Space: 59.5 MB (25.88%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,30 +2,26 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
         List<List<Integer>> list = new ArrayList<>();
         for(int i = 0; i < nums.length; i++){
-            if(i > 0 && nums[i-1] == nums[i])
-                continue;
-            int sum = nums[i];
-            int l = i + 1;
-            int r = nums.length - 1;
+            
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int l = i+1, r = nums.length - 1;
+
             while(l < r){
-                int addition = nums[l] + nums[r] + sum;
-                if(addition == 0){
-                    List<Integer> al = new ArrayList<>();
-                    al.add(nums[i]);al.add(nums[l]);al.add(nums[r]);
-                    list.add(al);
+
+                int sum = nums[l] + nums[r] + nums[i];
+                if(sum == 0){
+                    list.add(new ArrayList<>(List.of(nums[i], nums[l], nums[r])));
                     l++;
+                    while(l < nums.length && nums[l] == nums[l-1]) l++;
                     r--;
-                    while(l < r  && nums[l] == nums[l-1]) l++;
-                    while(l < r && nums[r] == nums[r + 1]) r--;    
-                    
-                }else if(addition < 0){
-                      l++;
+                    while(r >= 0 && nums[r] == nums[r+1]) r--;
                 }
-                else{
-                   r--;
+                else if(sum > 0){
+                    r--;
+                }else{
+                    l++;
                 }
             }
         }
