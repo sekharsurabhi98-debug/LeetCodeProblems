846efa27909931c888846fcce203dc8bb21296e7
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2026-03-12 16:31:39 +0530

    Time: 15 ms (23.75%), Space: 46.5 MB (39.86%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length() != t.length()) return false;
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+ 
+        for(char c : s.toCharArray()){
+            map1.put(c, map1.getOrDefault(c,0)+1);
+        }
+
+        for(char c : t.toCharArray()){
+            map2.put(c, map2.getOrDefault(c,0)+1);
+        }
+
+        for(char c : s.toCharArray()){
+            if(!map1.get(c).equals(map2.get(c)))
+            return false;
+        }
+        return true;
+    }
+}
