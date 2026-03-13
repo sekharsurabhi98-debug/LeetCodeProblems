407f65d749e15dd77ebff834a5650caac00304f9
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2026-03-13 21:20:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,26 +1,41 @@
 class Solution {
-    
-    public int expandToFindMaxLength(String s, int l, int r){
-        int maxLen = 1;
-        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+
+    public String checkAround(int i, String s, boolean isEven){
+        
+        int l , r;
+        if(isEven){
+            l = i;
+            r = i+1;
+        }else{
+            l = i - 1;
+            r =  i + 1;
+        }
+
+        while(l >= 0 && r < s.length()){
+            if(s.charAt(l) != s.charAt(r)){
+                break;
+            }
             l--;
             r++;
         }
-        System.out.println(r - l - 1);
-        return r - l - 1;
+        return s.substring(l+1, r);
     }
     public String longestPalindrome(String s){
-        int l = 0, r = 0, maxLength = 0, start = 0, end = 0;
-        for(int i = 0; i < s.length(); i++){
-            int odd = expandToFindMaxLength(s,i-1,i+1);
-            int even = expandToFindMaxLength(s,i,i+1);
-            int max = Math.max(odd,even);
-            if(maxLength < max){
-                maxLength = max;
-                start = i - (max - 1) / 2;
-                end = i + maxLength / 2;
+        
+        int start = 0;
+        int length = 0;
+        String result = "";
+        for(int i = 1; i < s.length(); i++){
+            
+            String even = checkAround(i,s,true);
+            String odd = checkAround(i,s,false);
+            if(even.length() > result.length()){
+                result = even;
+            }
+            if(odd.length() > result.length()){
+                result = odd;
             }
         }
-        return s.substring(start, end+1);
+        return result;
     }    
 }
