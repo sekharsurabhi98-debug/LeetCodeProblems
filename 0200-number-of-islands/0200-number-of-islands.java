c13cce19f3dfd2dfb5337e083b876d7c081e4c03
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2026-04-13 15:44:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,27 +2,28 @@ class Solution {
     public int numIslands(char[][] grid) {
 
         int count = 0;
+        int[] visited = new int[grid.length];
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == '1'){
+
+                if(grid[i][j] == '1' && visited[i] == 0){
                     count++;
-                    dfs(grid, i,j);
+                    dfs(grid, i, j, visited);
+                    
                 }
             }
         }
         return count;
     }
-
-    private void dfs(char[][] grid, int i, int j){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
-        return;
-        grid[i][j] = '0';
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
+    private void dfs(char[][] grid, int i, int j,int[] visited){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0'||visited[i] == 1)
+            return;
+        visited[i] = 1;
+        dfs(grid, i+1, j,visited);
+        dfs(grid, i-1, j,visited);
+        dfs(grid, i, j+1,visited);
+        dfs(grid, i, j-1,visited);
     }
- 
 
     private void bfs(char[][] grid, int i, int j){
         Queue<int[]> queue = new LinkedList<>();
