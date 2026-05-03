9cb65716e4d29c31b546b60faa86c99e20be2d28
sekharsurabhi98-debug <sekharsurabhi98@gmail.com>
2026-05-03 12:19:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,22 +1,26 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
         
-        StringBuilder sb = new StringBuilder();
-        sb.append(s);
-        int count = 0;
-        while(count < s.length()){
-            if(sb.toString().equals(goal)){
-                return true;
-            }
+        if(s.length() < goal.length()) return false;
+        // StringBuilder sb = new StringBuilder();
+        // sb.append(s);
+        // int count = 0;
 
-            sb.deleteCharAt(0);
-            sb.append(s.charAt(count));
+        // while(count < s.length()){
+        //     if(sb.toString().equals(goal)){
+        //         return true;
+        //     }
 
-            System.out.println(sb.toString());
-            count++;
-        }
+        //     sb.deleteCharAt(0);
+        //     sb.append(s.charAt(count));
 
-        return false;
+        //     System.out.println(sb.toString());
+        //     count++;
+        // }
+
+        String doubleString = s + s;
+
+        return doubleString.contains(goal);
         
     }
 }
